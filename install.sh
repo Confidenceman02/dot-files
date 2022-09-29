8730bd27f7377fb480923a93a46ee9da2f352417
Jaime Terreu <jaime@Jaimes-MBP.lan>
2022-09-29 22:38:52 +0930

    Add alacritty and lua helper

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 cp vimrc ~/.vimrc
-cp coc-settings.json ~/.config/nvim
-cp coc.vim ~/.vim/plugged/coc.nvim/
+# cp coc-settings.json ~/.config/nvim
+# cp coc.vim ~/.vim/plugged/coc.nvim/
 cp tmux.conf ~/.tmux.conf
+cp alacritty.yml ~/.config/alacritty
 cp -R after ~/.config/nvim
+cp confidenceman02.lua ~/.config/nvim/lua
