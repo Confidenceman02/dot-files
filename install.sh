05cc02922348adc7b6a720c36b8a0488e7c451d2
Jaime Terreu <jaime@terreu.com>
2022-10-23 13:56:53 +1030

    Create lua dirs
    We are using a custom directory for all lua files so this needs to be
    manual for now. For some reason nvim doesn't like lua files being in the
    autoload directory.

--- install.sh
+++ install.sh
@@ -10,3 +10,10 @@ cp tmux.conf ~/.tmux.conf
 cp alacritty.yml ~/.config/alacritty
 cp -R after ~/.config/nvim
 cp confidenceman02.lua ~/.config/nvim/lua
+
+# Setup lua dirs
+mkdir ~/.config/nvim/lua/lspconfig
+mkdir ~/.config/nvim/lua/nvim-cmp
+mkdir ~/.config/nvim/lua/cmp
+mkdir ~/.config/nvim/lua/luasnip
+mkdir ~/.config/nvim/lua/cmp_nvim_lsp
