d043caacc89072afb5828cd5ab301dad0643d50f
Jaime Terreu <jaime@terreu.com>
2022-10-23 11:29:16 +1030

    add init.vim config

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env zsh
 mkdir ~/.config/nvim
+mkdir ~/.config/nvim/lua
 mkdir ~/.config/alacritty
 cp vimrc ~/.vimrc
 # cp coc-settings.json ~/.config/nvim
 # cp coc.vim ~/.vim/plugged/coc.nvim/
+cp init.vim ~/.config/nvim
 cp tmux.conf ~/.tmux.conf
 cp alacritty.yml ~/.config/alacritty
 cp -R after ~/.config/nvim
