a1fc88e46ea07cd5918c3d844cd84afda7b3665d
Jaime Terreu <jaime@terreu.com>
2024-02-02 09:49:21 +1030

    nvim adjustments

--- lua/confidenceman02/plugins/nvim-tree.lua
+++ lua/confidenceman02/plugins/nvim-tree.lua
@@ -1,54 +1,54 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local nvimtree = require("nvim-tree")
+	dir = "~/code/nvim-tree.lua/",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local nvimtree = require("nvim-tree")
 
-    -- recommended settings from nvim-tree documentation
-    vim.g.loaded = 1
-    vim.g.loaded_netrwPlugin = 1
+		-- recommended settings from nvim-tree documentation
+		vim.g.loaded = 1
+		vim.g.loaded_netrwPlugin = 1
 
-    -- change color for arrows in tree to light blue
-    vim.cmd([[highlight NvimTreeIndentMarker guifg=#3FC5FF]])
+		-- change color for arrows in tree to light blue
+		vim.cmd([[highlight NvimTreeIndentMarker guifg=#3FC5FF]])
 
-    nvimtree.setup({
-      view = {
-        width = 60,
-      },
-      -- change folder arrow icons
-      renderer = {
-        icons = {
-          glyphs = {
-            folder = {
-              arrow_closed = "🡪", -- arrow when folder is closed
-              arrow_open = "🡫", -- arrow when folder is open
-            },
-          },
-        },
-      },
-      -- disable window_picker for
-      -- exporer to work well with
-      -- window splits
-      actions = {
-        open_file = {
-          window_picker = {
-            enable = false,
-          },
-        },
-      },
-      filters = {
-        custom = { ".DS_Store" },
-      },
-      git = {
-        ignore = false,
-      },
-    })
+		nvimtree.setup({
+			view = {
+				width = 60,
+			},
+			-- change folder arrow icons
+			renderer = {
+				icons = {
+					glyphs = {
+						folder = {
+							arrow_closed = "🡪", -- arrow when folder is closed
+							arrow_open = "🡫", -- arrow when folder is open
+						},
+					},
+				},
+			},
+			-- disable window_picker for
+			-- exporer to work well with
+			-- window splits
+			actions = {
+				open_file = {
+					window_picker = {
+						enable = false,
+					},
+				},
+			},
+			filters = {
+				custom = { ".DS_Store" },
+			},
+			git = {
+				ignore = false,
+			},
+		})
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>") -- toggle file explorer
-    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>") -- collapse file explorer
-    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>") -- refresh file explorer
-  end
-  }
+		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>") -- toggle file explorer
+		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>") -- collapse file explorer
+		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>") -- refresh file explorer
+	end,
+}
