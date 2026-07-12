dc8f13b1e4a4db4bdafac436333450bfeb88455d
Confidenceman02 <25443812+Confidenceman02@users.noreply.github.com>
2026-07-13 02:05:40 +0930

    Update formatting.lua

---
+++ lua/config/plugins/formatting.lua
@@ -0,0 +1,22 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				graphql = { "prettier" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+			},
+		})
+	end,
+}
